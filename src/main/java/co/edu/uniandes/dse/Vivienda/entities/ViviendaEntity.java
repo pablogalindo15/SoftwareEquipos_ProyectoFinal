4aae1f31abb7ca4f66883807f51ffa6cff0923e2
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-02-07 08:47:12 -0500

    viviendasEntity

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/entities/ViviendaEntity.java
@@ -0,0 +1,32 @@
+package co.edu.uniandes.dse.Vivienda.entities;
+
+import javax.persistence.Entity;
+
+import org.hibernate.mapping.Collection;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class ViviendaEntity extends BaseEntity{
+    
+    private String nombre;
+    private float precio;
+    private String descripcion;
+    private String fotos;
+    private int tamano;
+    private int estrato;
+    private String restricciones;
+    public enum tipoVivienda{
+        apartaestudio, apartamentoComp, habitaconEnApto, habitacionFamilia
+    };
+    private String contacto;
+    private String direccion;
+    private Boolean ocupada;
+    private double coordX;
+    private double coordY;
+
+
+
+
+}
