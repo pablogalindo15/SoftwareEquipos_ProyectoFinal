d515e77f1fb83d706d745c9f854e63be47d5621b
pablogalindo15 <92609746+pablogalindo15@users.noreply.github.com>
2024-02-11 22:18:04 -0500

    Persistencia Relaciones Servicio

--- src/main/java/co/edu/uniandes/dse/Vivienda/entities/ServicioEntity.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/entities/ServicioEntity.java
@@ -1,15 +1,22 @@
 package co.edu.uniandes.dse.Vivienda.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
 @Data
 @Entity
 public class ServicioEntity extends BaseEntity {
+
     private String nombre;
     private float costoAdicional;
-    public enum tipoServicio {parqueadero, gimansio, lavanderia, deposito, restaurante};
+    public enum tipoServicio {
+        parqueadero, gimansio, lavanderia, deposito, restaurante
+    };
+    
+    @ManyToOne
+    ViviendaEntity vivienda;
 
     
 }
