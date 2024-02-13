7cbd4e0c8a51c1f296f39b2f31ee36555b52d0d2
mh783 <142255399+mh783@users.noreply.github.com>
2024-02-12 20:44:03 -0500

    Relacion estudiantes_comentarios

--- src/main/java/co/edu/uniandes/dse/Vivienda/entities/ComentarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/entities/ComentarioEntity.java
@@ -1,15 +1,25 @@
 package co.edu.uniandes.dse.Vivienda.entities;
 
+import java.util.ArrayList;
+
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
+
 @Data
 @Entity
 public class ComentarioEntity extends  BaseEntity
 {
+    
     private String titulo;
     private String texto;
     private int calificacion;
+    @Id
     private String nombre;
-
+    @ManyToMany(mappedBy = "comentarios")
+    private List<HabitanteEntity> estudiantes = new ArrayList<>();
+    
 }
--- src/main/java/co/edu/uniandes/dse/Vivienda/entities/HabitanteEntity.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/entities/HabitanteEntity.java
@@ -1,14 +1,22 @@
 package co.edu.uniandes.dse.Vivienda.entities;
 
+import java.util.ArrayList;
+
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
 @Data
 @Entity
 public class HabitanteEntity extends BaseEntity {
+     @Id
      private String nombre;
+     
      private int cedula;
 
-    
+@ManyToMany
+private List<ComentarioEntity> comentarios = new ArrayList<>();
 }
