651574b344804455d30b2f3a768a3b2f1d2e8aeb
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-02-14 08:06:44 -0500

    vivienda services

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/services/ViviendaService.java
@@ -0,0 +1,13 @@
+package co.edu.uniandes.dse.Vivienda.services;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.Vivienda.entities.ViviendaEntity;
+
+@Service
+public class ViviendaService {
+    public ViviendaEntity findVivienda(){
+        
+    }
+    
+}
