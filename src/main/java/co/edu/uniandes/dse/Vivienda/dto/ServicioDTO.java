4c9c99e04c1b4f1a6271dcd8cea7dfb00194de67
pablogalindo15 <92609746+pablogalindo15@users.noreply.github.com>
2024-02-20 09:25:27 -0500

    ServicioDTO

--- src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDTO.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDTO.java
@@ -1,5 +1,12 @@
 package co.edu.uniandes.dse.Vivienda.dto;
 
 public class ServicioDTO {
-    
+    public String nombre;
+   
+    public Float costoAdicional;
+
+    public enum tipoServicio {parqueadero, gimansio, lavanderia, deposito, restaurante};
+
+    public tipoServicio tipServicio;
 }
+
--- src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDetailDTO.java
@@ -1,5 +1,11 @@
 package co.edu.uniandes.dse.Vivienda.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import co.edu.uniandes.dse.Vivienda.entities.ServicioEntity;
+
 public class ServicioDetailDTO {
+    private List<ServicioEntity> viviendasServicio = new ArrayList<>();
     
 }
