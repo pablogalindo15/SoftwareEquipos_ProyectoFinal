d7b2e3dc8a0a8de1427a4a72a0bbaf7de3b4c075
Nicolas Godoy <n.godoy@uniandes.edu.co>
2024-02-19 23:26:27 -0500

    Implementacion Logica y Pruebas Nicolas

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/services/HabitanteService.java
@@ -0,0 +1,65 @@
+package co.edu.uniandes.dse.Vivienda.services;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import org.assertj.core.configuration.Services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import co.edu.uniandes.dse.Vivienda.entities.HabitanteEntity;
+import co.edu.uniandes.dse.Vivienda.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.Vivienda.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.Vivienda.repositories.HabitanteRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service 
+public class HabitanteService {
+
+    @Autowired
+    private HabitanteRepository HabitanteRepository;
+    @Transactional
+    public HabitanteEntity createHabitante(HabitanteEntity habitante)throws EntityNotFoundException, IllegalOperationException {
+        log.info("Inicia proceso de creación de habitante");
+
+        if (habitante.getNombre()==null){
+            throw new IllegalOperationException("El nombre no es valido");}
+        if (habitante.getCedula() == 0){
+            throw new IllegalOperationException("La cedula no es valida");}
+        HabitanteEntity newHabitanteEntity = HabitanteRepository.save(habitante);
+        log.info("Termina proceso de creación de habitante");
+        return newHabitanteEntity;
+    }
+    public HabitanteEntity getHabitante(Long habitanteId)throws EntityNotFoundException{
+        log.info("Inicia proceso de consultar el habitante con id = {0}", habitanteId);
+        Optional<HabitanteEntity> HabitanteEntity = HabitanteRepository.findById(habitanteId);
+        if (HabitanteRepository.findById(habitanteId).isEmpty()){
+            throw new EntityNotFoundException("No se encontro el habitante");
+        }
+        log.info("Termina proceso de consultar el habitante con id = {0}", habitanteId);
+        return HabitanteEntity.get();
+    }
+    public HabitanteEntity updateHabitante(Long habitanteId, HabitanteEntity habitante)throws EntityNotFoundException, IllegalOperationException{
+        log.info("Inicia proceso de actualizar el habitante con id = {0}", habitanteId);
+        if (HabitanteRepository.findById(habitanteId).isEmpty()){
+            throw new EntityNotFoundException("No se encontro el habitante");
+        }
+        if (habitante.getNombre()==null){
+            throw new IllegalOperationException("El nombre no es valido");}
+        HabitanteEntity newHabitanteEntity = HabitanteRepository.save(habitante);
+        log.info("Termina proceso de actualizar el habitante con id = {0}", habitanteId);
+        return newHabitanteEntity;
+    }
+    public void deleteHabitante(Long habitanteId)throws EntityNotFoundException{
+        log.info("Inicia proceso de borrar el habitante con id = {0}", habitanteId);
+        if (HabitanteRepository.findById(habitanteId).isEmpty()){
+            throw new EntityNotFoundException("No se encontro el habitante");
+        }
+        HabitanteRepository.deleteById(habitanteId);
+        log.info("Termina proceso de borrar el habitante con id = {0}", habitanteId);
+    }
+
+
+    
+
+    
+    
+}
---
+++ src/test/java/co/edu/uniandes/dse/Vivienda/services/HabitanteServiceTest.java
@@ -0,0 +1,193 @@
+package co.edu.uniandes.dse.Vivienda.services;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import org.assertj.core.configuration.Services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import lombok.Data;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import co.edu.uniandes.dse.Vivienda.entities.HabitanteEntity;
+import co.edu.uniandes.dse.Vivienda.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.Vivienda.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.Vivienda.repositories.HabitanteRepository;
+import lombok.extern.slf4j.Slf4j;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+
+
+@DataJpaTest
+@Transactional
+@Import(HabitanteService.class)
+class HabitanteServiceTest {
+    @Autowired
+    private HabitanteService habitanteService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private List<HabitanteEntity> habitanteList = new ArrayList<>();
+    private PodamFactory factory = new PodamFactoryImpl();
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @BeforeEach
+    void setUp() {
+        clearData();
+        insertData();
+        }
+
+    /**
+    * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        entityManager.getEntityManager().createQuery("delete from HabitanteEntity");
+        }
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las pruebas.
+     */
+    private void insertData() { 
+        for (int i = 0; i < 3; i++) {
+            HabitanteEntity habitante = factory.manufacturePojo(HabitanteEntity.class);
+            entityManager.persist(habitante);
+            habitanteList.add(habitante);
+        }
+    
+        HabitanteEntity HabitanteEntity = factory.manufacturePojo(HabitanteEntity.class);
+            entityManager.persist(HabitanteEntity);
+            HabitanteEntity.getNombre().add(habitanteList.get(0));
+            habitanteList.get(0).getNombre().add(HabitanteEntity);
+        
+    }
+    @Test
+    void createHabitanteTest() throws EntityNotFoundException, IllegalOperationException {
+        HabitanteEntity newEntity = factory.manufacturePojo(HabitanteEntity.class);
+        HabitanteEntity result = habitanteService.createHabitante(newEntity);
+        assertNotNull(result);
+        HabitanteEntity entity = entityManager.find(HabitanteEntity.class, result.getId());
+        assertNotNull(entity);
+        assertEquals(newEntity.getId(), entity.getId());
+    }
+    @Test
+    void testCreateHabitanteWithNoValidName() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity newEntity = factory.manufacturePojo(HabitanteEntity.class);
+            newEntity.setNombre("");
+            habitanteService.createHabitante(newEntity);
+        });
+    }
+    @Test
+    void testCreateHabitanteWithNoValidName2() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity newEntity = factory.manufacturePojo(HabitanteEntity.class);
+            newEntity.setNombre(null);
+            habitanteService.createHabitante(newEntity);
+        });
+    }
+    @Test
+    void testCreateHabitanteWithStoredId() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity newEntity = factory.manufacturePojo(HabitanteEntity.class);
+            newEntity.setId(habitanteList.get(0).getId());
+            habitanteService.createHabitante(newEntity);
+        });
+    }
+    @Test
+    void testGetHabitantes() {
+        List<HabitanteEntity> list = habitanteService.getHabitantes();
+        assertEquals(habitanteList.size(), list.size());
+        for (HabitanteEntity entity : list) {
+            boolean found = false;
+            for (HabitanteEntity storedEntity : habitanteList) {
+                if (entity.getId().equals(storedEntity.getId())) {
+                    found = true;
+                }
+            }
+            assertTrue(found);
+        }
+
+    }
+    @Test
+    void testGetHabitante() throws EntityNotFoundException {
+        HabitanteEntity entity = habitanteList.get(0);
+        HabitanteEntity resultEntity = habitanteService.getHabitante(entity.getId());
+        assertNotNull(resultEntity);
+        assertEquals(entity.getId(), resultEntity.getId());
+        assertEquals(entity.getNombre(), resultEntity.getNombre());
+    }
+    @Test
+    void testGetInvalidHabitante() {
+        assertThrows(EntityNotFoundException.class, () -> {
+            habitanteService.getHabitante(0L);
+        });
+    }
+    @Test
+    void testUpdateHabitante() throws EntityNotFoundException, IllegalOperationException {
+        HabitanteEntity entity = habitanteList.get(0);
+        HabitanteEntity pojoEntity = factory.manufacturePojo(HabitanteEntity.class);
+        pojoEntity.setId(entity.getId());
+        habitanteService.updateHabitante(pojoEntity.getId(), pojoEntity);
+        HabitanteEntity resp = entityManager.find(HabitanteEntity.class, entity.getId());
+        assertEquals(pojoEntity.getId(), resp.getId());
+        assertEquals(pojoEntity.getNombre(), resp.getNombre());
+    }
+    @Test
+    void testUpdateHabitanteInvalid() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity pojoEntity = factory.manufacturePojo(HabitanteEntity.class);
+            pojoEntity.setId(0L);
+            habitanteService.updateHabitante(0L, pojoEntity);
+        });
+    }
+    @Test
+    void testUpdateHabitanteWithNoValidName() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity entity = habitanteList.get(0);
+            HabitanteEntity pojoEntity = factory.manufacturePojo(HabitanteEntity.class);
+            pojoEntity.setNombre("");
+            pojoEntity.setId(entity.getId());
+            habitanteService.updateHabitante(pojoEntity.getId(), pojoEntity);
+        });
+    }
+    @Test
+    void testUpdateHabitanteWithNoValidName2() {
+        assertThrows(IllegalOperationException.class, () -> {
+            HabitanteEntity entity = habitanteList.get(0);
+            HabitanteEntity pojoEntity = factory.manufacturePojo(HabitanteEntity.class);
+            pojoEntity.setNombre(null);
+            pojoEntity.setId(entity.getId());
+            habitanteService.updateHabitante(pojoEntity.getId(), pojoEntity);
+        });
+    }
+    @Test
+    void testDeleteHabitante() throws EntityNotFoundException {
+        HabitanteEntity entity = habitanteList.get(1);
+        habitanteService.deleteHabitante(entity.getId());
+        HabitanteEntity deleted = entityManager.find(HabitanteEntity.class, entity.getId());
+        assertNull(deleted);
+    }
+    @Test
+    void testDeleteHabitanteInvalid() {
+        assertThrows(EntityNotFoundException.class, () -> {
+            habitanteService.deleteHabitante(0L);
+        });
+    }
+
+
+
+}
