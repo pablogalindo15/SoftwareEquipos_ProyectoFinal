1e9aeb80f3a43ed990b4269a9dfc9f7802081ccc
Nicolas Godoy <n.godoy@uniandes.edu.co>
2024-02-20 09:23:18 -0500

    HabitanteDTO

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/HabitanteDTO.java
@@ -0,0 +1,11 @@
+package co.edu.uniandes.dse.Vivienda.dto;
+
+import lombok.Data;
+
+@Data
+public class HabitanteDTO {
+    private Long id;
+    private String nombre;
+    private int cedula;
+    
+}
