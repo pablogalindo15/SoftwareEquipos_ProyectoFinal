51dcad59f8b8b712fa21702a3cb75174c4a3a40e
julianaduranl <123585846+julianaduranl@users.noreply.github.com>
2024-02-20 09:20:29 -0500

    Lugar DTO

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/LugarDTO.java
@@ -0,0 +1,17 @@
+package co.edu.uniandes.dse.Vivienda.dto;
+import lombok.Data;
+
+@Data
+public class LugarDTO {
+    private Long id;
+    private String nombre;
+    private String fotos;
+    private int tiempoLlegada;
+    private boolean gratis;
+    private Double precioMin;
+    private Double precioMax;
+    private Double coordenadaX;
+    private Double coordenadaY;
+    public enum tipoLugar {Universidad, Restaurante, Supermercado, Parque, CentroComercial }
+    private tipoLugar tipo;
+}
