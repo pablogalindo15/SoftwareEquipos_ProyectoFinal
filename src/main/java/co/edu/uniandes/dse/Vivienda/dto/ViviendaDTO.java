f81d57533bace8120dd87c4d0d4ed653c7889474
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-02-20 09:19:19 -0500

    dto vivienda

---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/ViviendaDTO.java
@@ -0,0 +1,28 @@
+package co.edu.uniandes.dse.Vivienda.dto;
+
+import lombok.Data;
+
+@Data
+public class ViviendaDTO {
+    private Long id;
+    private String nombre;
+    private float precio;
+    private String descripcion;
+    private String fotos;
+    private Integer tamano;
+    public enum posiblesEstratos{
+        uno,  dos, tres, cuatro, cinco, seis
+    };
+    private posiblesEstratos estrato;
+    private String restricciones;
+    public enum tipoVivienda{
+        apartaestudio, apartamentoComp, habitaconEnApto, habitacionFamilia
+    };
+    private tipoVivienda tipo;
+    private String contacto;
+    private String direccion;
+    private Boolean ocupada;
+    private double coordX;
+    private double coordY;
+
+}
---
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/ViviendaDetailDTO.java
@@ -0,0 +1,5 @@
+package co.edu.uniandes.dse.Vivienda.dto;
+
+public class ViviendaDetailDTO {
+    
+}
