b139f758943376177f62b099532a39817c3c36bd
Jeronimo <j.francoc2@uniandes.edu.co>
2024-02-21 08:59:06 -0500

    DTO'S Propietario

--- src/main/java/co/edu/uniandes/dse/Vivienda/dto/PropietarioDTO.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/PropietarioDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class PropietarioDTO {
-    private long id; 
+    private Long id; 
     private String foto;
     private String nombre; 
     private Integer celular;
--- src/main/java/co/edu/uniandes/dse/Vivienda/dto/PropietarioDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/PropietarioDetailDTO.java
@@ -1,5 +1,14 @@
 package co.edu.uniandes.dse.Vivienda.dto;
 
-public class PropietarioDetailDTO {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+import co.edu.uniandes.dse.Vivienda.entities.ViviendaEntity;
+
+public class PropietarioDetailDTO extends PropietarioDTO {
+        @OneToMany(mappedBy="propietario")
+        private List<ViviendaEntity> viviendas = new ArrayList<>(); 
+
 }
