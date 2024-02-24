2f1516d0d7bd922b823596a265a23cd337989a2f
pablogalindo15 <92609746+pablogalindo15@users.noreply.github.com>
2024-02-23 23:25:29 -0500

    DETAILDTO

--- src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/dto/ServicioDetailDTO.java
@@ -3,11 +3,10 @@ package co.edu.uniandes.dse.Vivienda.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import co.edu.uniandes.dse.Vivienda.entities.ServicioEntity;
 import lombok.Data;
 @Data
 public class ServicioDetailDTO extends ServicioDTO{
-    private List<ServicioEntity> viviendasServicio = new ArrayList<>();
+    private List<ViviendaDTO> viviendasServicio = new ArrayList<>();
     
     
 }
