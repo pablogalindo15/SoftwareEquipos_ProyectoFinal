cabad78c195b1e56cbd9f83e92d44edf597eba19
mh783 <142255399+mh783@users.noreply.github.com>
2024-03-03 13:58:16 -0500

    Comentario API

--- src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ComentarioController.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ComentarioController.java
@@ -4,8 +4,11 @@ package co.edu.uniandes.dse.Vivienda.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import co.edu.uniandes.dse.Vivienda.entities.ComentarioEntity;
+import co.edu.uniandes.dse.Vivienda.entities.ViviendaEntity;
 import co.edu.uniandes.dse.Vivienda.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.Vivienda.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.Vivienda.services.ComentarioService;
@@ -20,6 +24,8 @@ import org.modelmapper.TypeToken;
 
 import co.edu.uniandes.dse.Vivienda.dto.ComentarioDTO;
 import co.edu.uniandes.dse.Vivienda.dto.ComentarioDetailDTO;
+import co.edu.uniandes.dse.Vivienda.dto.ViviendaDTO;
+import co.edu.uniandes.dse.Vivienda.dto.ViviendaDetailDTO;
 @RestController
 @RequestMapping("/comentario")
 public class ComentarioController {
@@ -42,4 +48,23 @@ public List<ComentarioDetailDTO> findall(){
 		 ComentarioEntity comentarioEntity = comentarioService.crear(modelMapper.map( comentarioDTO, ComentarioEntity.class));
 		return modelMapper.map(comentarioEntity, ComentarioDTO.class);
 	}
+@GetMapping(value = "{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ComentarioDTO findOne(@PathVariable("id")Long id) throws EntityNotFoundException, IllegalOperationException{
+        ComentarioEntity comentarioEntity = comentarioService.getComentario(id);
+        return modelMapper.map(comentarioEntity, ComentarioDetailDTO.class);
+
+    }
+	@PutMapping(value = "{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ComentarioDTO update(@PathVariable("id")Long id, @RequestBody ComentarioDTO comentarioDTO) throws EntityNotFoundException, IllegalOperationException{
+        ComentarioEntity comentarioEntity = comentarioService.updateComentario(id, modelMapper.map(comentarioDTO, ComentarioEntity.class));
+        return modelMapper.map(comentarioEntity, ComentarioDTO.class);
+    }
+	@DeleteMapping(value = "{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void delete(@PathVariable("id")Long id) throws EntityNotFoundException, IllegalOperationException{
+        comentarioService.deleteComentario(id);;
+
+    }
 }
