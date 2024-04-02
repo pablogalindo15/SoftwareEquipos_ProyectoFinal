54a76f7bbfe4c29757eafaed4e9dbefe64f89a4c
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-04-02 06:58:56 -0500

    intento 2. todas las relaciones de vivienda y lugares

--- src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ViviendaHabitante2Controller.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ViviendaHabitante2Controller.java
@@ -1,67 +1,67 @@
-// package co.edu.uniandes.dse.Vivienda.controllers;
+package co.edu.uniandes.dse.Vivienda.controllers;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.modelmapper.ModelMapper;
-// import org.modelmapper.TypeToken;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.*;
-// import co.edu.uniandes.dse.Vivienda.dto.*;
-// import co.edu.uniandes.dse.Vivienda.entities.HabitanteEntity;
-// import co.edu.uniandes.dse.Vivienda.exceptions.*;
-// import co.edu.uniandes.dse.Vivienda.services.ViviendaHabitante2Service;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import co.edu.uniandes.dse.Vivienda.dto.*;
+import co.edu.uniandes.dse.Vivienda.entities.HabitanteEntity;
+import co.edu.uniandes.dse.Vivienda.exceptions.*;
+import co.edu.uniandes.dse.Vivienda.services.ViviendaHabitante2Service;
 
-// @RestController
-// @RequestMapping("/viviendas")
-// public class ViviendaHabitante2Controller {
+@RestController
+@RequestMapping("/viviendas")
+public class ViviendaHabitante2Controller {
     
-//      @Autowired
-//     private ViviendaHabitante2Service viviendaHabitanteService;
+     @Autowired
+    private ViviendaHabitante2Service viviendaHabitanteService;
 
-//     @Autowired
-//     private ModelMapper modelMapper;
+    @Autowired
+    private ModelMapper modelMapper;
 
-//     @PostMapping("/{viviendaId}/historial/{habitanteId}")
-//     public HabitanteDetailDTO addHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException {
-//         HabitanteEntity habitanteEntity = viviendaHabitanteService.addHabitante(viviendaId, habitanteId);
-//         return modelMapper.map(habitanteEntity, HabitanteDetailDTO.class);
-//     }
+    @PostMapping("/{viviendaId}/historial/{habitanteId}")
+    public HabitanteDetailDTO addHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException {
+        HabitanteEntity habitanteEntity = viviendaHabitanteService.addHabitante(viviendaId, habitanteId);
+        return modelMapper.map(habitanteEntity, HabitanteDetailDTO.class);
+    }
 
-//     @GetMapping("/{viviendaId}/historial/{habitanteId}")
-//     @ResponseStatus(code = HttpStatus.OK)
-//     public HabitanteDetailDTO getHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException, IllegalOperationException {
-//             HabitanteEntity habitanteEntity= viviendaHabitanteService.getHabitante(viviendaId, habitanteId);
-//         return modelMapper.map(habitanteEntity, HabitanteDetailDTO.class);
-//     }
+    @GetMapping("/{viviendaId}/historial/{habitanteId}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public HabitanteDetailDTO getHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException, IllegalOperationException {
+            HabitanteEntity habitanteEntity= viviendaHabitanteService.getHabitante(viviendaId, habitanteId);
+        return modelMapper.map(habitanteEntity, HabitanteDetailDTO.class);
+    }
 
-//     @GetMapping("/{viviendaId}/historial")
-//     @ResponseStatus(code = HttpStatus.OK)
-//     public List<HabitanteEntity> getHabitantes2(@PathVariable("viviendaId") Long viviendaId) throws EntityNotFoundException {
-//         List<HabitanteEntity> habitanteEntity = viviendaHabitanteService.getHabitantes(viviendaId);
+    @GetMapping("/{viviendaId}/historial")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<HabitanteEntity> getHabitantes2(@PathVariable("viviendaId") Long viviendaId) throws EntityNotFoundException {
+        List<HabitanteEntity> habitanteEntity = viviendaHabitanteService.getHabitantes(viviendaId);
         
-//         return modelMapper.map(habitanteEntity,  new TypeToken<List<HabitanteDetailDTO>>() {
-// 		}.getType());
-//     }
+        return modelMapper.map(habitanteEntity,  new TypeToken<List<HabitanteDetailDTO>>() {
+		}.getType());
+    }
 
-//     @PutMapping("/{viviendaId}/historial")
-//     public List<HabitanteDetailDTO> replaceHabitante2(@PathVariable("viviendaId") Long viviendaId, @RequestBody List<HabitanteDTO> habitantes) throws EntityNotFoundException {
+    @PutMapping("/{viviendaId}/historial")
+    public List<HabitanteDetailDTO> replaceHabitante2(@PathVariable("viviendaId") Long viviendaId, @RequestBody List<HabitanteDTO> habitantes) throws EntityNotFoundException {
 
-//         List<HabitanteEntity> entities = modelMapper.map(habitantes, new TypeToken<List<HabitanteEntity>>() {
-// 		}.getType());
+        List<HabitanteEntity> entities = modelMapper.map(habitantes, new TypeToken<List<HabitanteEntity>>() {
+		}.getType());
 
-//         List<HabitanteEntity> habitantesList = viviendaHabitanteService.replaceHabitantes(viviendaId, entities);
+        List<HabitanteEntity> habitantesList = viviendaHabitanteService.replaceHabitantes(viviendaId, entities);
 
-//         return modelMapper.map(habitantesList,  new TypeToken<List<HabitanteDetailDTO>>() {
-// 		}.getType());
-//     }
+        return modelMapper.map(habitantesList,  new TypeToken<List<HabitanteDetailDTO>>() {
+		}.getType());
+    }
 
     
-//     @DeleteMapping("/{viviendaId}/historial/{habitanteId}")
-//     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-//     public void removeHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException{
-//         viviendaHabitanteService.removeHabitante(viviendaId, habitanteId);
-//     }
+    @DeleteMapping("/{viviendaId}/historial/{habitanteId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void removeHabitante2(@PathVariable("viviendaId") Long viviendaId, @PathVariable("habitanteId") Long habitanteId) throws EntityNotFoundException{
+        viviendaHabitanteService.removeHabitante(viviendaId, habitanteId);
+    }
 
 
-// }
+}
--- src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ViviendaServicioController.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/controllers/ViviendaServicioController.java
@@ -1,76 +1,76 @@
-// package co.edu.uniandes.dse.Vivienda.controllers;
+package co.edu.uniandes.dse.Vivienda.controllers;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.modelmapper.ModelMapper;
-// import org.modelmapper.TypeToken;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.ResponseStatus;
-// import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-// import co.edu.uniandes.dse.Vivienda.dto.ServicioDTO;
-// import co.edu.uniandes.dse.Vivienda.dto.ServicioDetailDTO;
-// import co.edu.uniandes.dse.Vivienda.entities.ServicioEntity;
-// import co.edu.uniandes.dse.Vivienda.exceptions.EntityNotFoundException;
-// import co.edu.uniandes.dse.Vivienda.exceptions.IllegalOperationException;
-// import co.edu.uniandes.dse.Vivienda.services.ViviendaServicioService;
+import co.edu.uniandes.dse.Vivienda.dto.ServicioDTO;
+import co.edu.uniandes.dse.Vivienda.dto.ServicioDetailDTO;
+import co.edu.uniandes.dse.Vivienda.entities.ServicioEntity;
+import co.edu.uniandes.dse.Vivienda.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.Vivienda.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.Vivienda.services.ViviendaServicioService;
 
-// @RestController
-// @RequestMapping("/viviendas")
-// public class ViviendaServicioController {
+@RestController
+@RequestMapping("/viviendas")
+public class ViviendaServicioController {
 
-//     @Autowired
-//     private ViviendaServicioService viviendaServicioService;
+    @Autowired
+    private ViviendaServicioService viviendaServicioService;
 
-//     @Autowired
-//     private ModelMapper modelMapper;
+    @Autowired
+    private ModelMapper modelMapper;
     
-//     @PostMapping("/{viviendaId}/servicios/{servicioId}")
-//     public ServicioDetailDTO addServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException {
-//         ServicioEntity servicioEntity = viviendaServicioService.addServicio(viviendaId, servicioId);
-//         return modelMapper.map(servicioEntity, ServicioDetailDTO.class);
-//     }
+    @PostMapping("/{viviendaId}/servicios/{servicioId}")
+    public ServicioDetailDTO addServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException {
+        ServicioEntity servicioEntity = viviendaServicioService.addServicio(viviendaId, servicioId);
+        return modelMapper.map(servicioEntity, ServicioDetailDTO.class);
+    }
 
-//     @GetMapping("/{viviendaId}/servicios/{servicioId}")
-//     @ResponseStatus(code = HttpStatus.OK)
-//     public ServicioDetailDTO getServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException, IllegalOperationException {
-//             ServicioEntity servicioEntity= viviendaServicioService.getServicio(viviendaId, servicioId);
-//         return modelMapper.map(servicioEntity, ServicioDetailDTO.class);
-//     }
+    @GetMapping("/{viviendaId}/servicios/{servicioId}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ServicioDetailDTO getServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException, IllegalOperationException {
+            ServicioEntity servicioEntity= viviendaServicioService.getServicio(viviendaId, servicioId);
+        return modelMapper.map(servicioEntity, ServicioDetailDTO.class);
+    }
 
-//     @GetMapping("/{viviendaId}/servicios")
-//     @ResponseStatus(code = HttpStatus.OK)
-//     public List<ServicioEntity> getServicios(@PathVariable("viviendaId") Long viviendaId) throws EntityNotFoundException {
-//         List<ServicioEntity> servicioEntity = viviendaServicioService.getServicios(viviendaId);
-//         return modelMapper.map(servicioEntity,  new TypeToken<List<ServicioDetailDTO>>() {
-// 		}.getType());
-//     }
+    @GetMapping("/{viviendaId}/servicios")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<ServicioEntity> getServicios(@PathVariable("viviendaId") Long viviendaId) throws EntityNotFoundException {
+        List<ServicioEntity> servicioEntity = viviendaServicioService.getServicios(viviendaId);
+        return modelMapper.map(servicioEntity,  new TypeToken<List<ServicioDetailDTO>>() {
+		}.getType());
+    }
 
     
-//     @PutMapping("/{viviendaId}/servicios")
-//     public List<ServicioDetailDTO> replaceServicios(@PathVariable("viviendaId") Long viviendaId, @RequestBody List<ServicioDTO> servicios) throws EntityNotFoundException {
+    @PutMapping("/{viviendaId}/servicios")
+    public List<ServicioDetailDTO> replaceServicios(@PathVariable("viviendaId") Long viviendaId, @RequestBody List<ServicioDTO> servicios) throws EntityNotFoundException {
 
-//         List<ServicioEntity> entities = modelMapper.map(servicios, new TypeToken<List<ServicioEntity>>() {
-// 		}.getType());
+        List<ServicioEntity> entities = modelMapper.map(servicios, new TypeToken<List<ServicioEntity>>() {
+		}.getType());
 
-//         List<ServicioEntity> serviciosList = viviendaServicioService.replaceServicios(viviendaId, entities);
+        List<ServicioEntity> serviciosList = viviendaServicioService.replaceServicios(viviendaId, entities);
 
-//         return modelMapper.map(serviciosList,  new TypeToken<List<ServicioDetailDTO>>() {
-// 		}.getType());
-//     }
+        return modelMapper.map(serviciosList,  new TypeToken<List<ServicioDetailDTO>>() {
+		}.getType());
+    }
     
-//     @DeleteMapping("/{viviendaId}/servicios/{servicioId}")
-//     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-//     public void removeServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException{
-//         viviendaServicioService.removeServicio(viviendaId, servicioId);
-//     }
+    @DeleteMapping("/{viviendaId}/servicios/{servicioId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void removeServicio(@PathVariable("viviendaId") Long viviendaId, @PathVariable("servicioId") Long servicioId) throws EntityNotFoundException{
+        viviendaServicioService.removeServicio(viviendaId, servicioId);
+    }
 
-// }
+}
