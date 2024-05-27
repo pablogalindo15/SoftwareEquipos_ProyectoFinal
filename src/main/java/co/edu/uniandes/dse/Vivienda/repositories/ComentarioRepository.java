6b1a1a78722d1c67694cf1008736f6a155a4f9cc
mh783 <m.hernandezr2@uniandes.edu.co>
2024-05-27 13:36:11 -0500

    .

--- src/main/java/co/edu/uniandes/dse/Vivienda/repositories/ComentarioRepository.java
+++ src/main/java/co/edu/uniandes/dse/Vivienda/repositories/ComentarioRepository.java
@@ -3,10 +3,11 @@ package co.edu.uniandes.dse.Vivienda.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import co.edu.uniandes.dse.Vivienda.entities.ComentarioEntity;
 
 @Repository
-public interface ComentarioRepository extends JpaRepository<ComentarioEntity,Long> {
+public interface ComentarioRepository extends JpaRepository<ComentarioEntity,Long>,PagingAndSortingRepository<ComentarioEntity, Long> {
     
 }
